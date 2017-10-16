f6e191d021f78e3f252daacc249ce0d4835dc1e2
Sulley01 <sullivan.alvin@ymail.com>
2017-10-16 16:50:03 +0700

    Created segment and ACK data type

---
+++ segment.h
@@ -0,0 +1,20 @@
+#ifndef SEGMENT_H
+#define SEGMENT_H
+
+typedef struct {
+    char soh;
+    int sequencenumber;
+    char stx;
+    char data;
+    char etx;
+    char checksum;
+} segment;
+
+typedef struct {
+    char ack;
+    int nextsequencenumber;
+    char advertisedwindowsize;
+    char checksum;
+} ack;
+
+#endif
